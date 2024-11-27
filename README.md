f6295f1176200ff7c88fc9c73ad66f6dfcde3de5
22wmr01714 <leepw-wm22@student.tarc.edu.my>
2024-11-27 15:41:13 +0800

    index page edited

--- README.md
+++ README.md
@@ -1,5 +1,37 @@
 # pay360client
 This project aims to ease our client to use our payment gateway API.
 
-Installation
-composer require pay-360/pay360client
+Installation..
+> cmd > composer require pay-360/pay360client
+
+After Install..
+> Move files (except for the pay360client.php) from x/vendor/pay-360/pay360client/src/ to x(main folder)
+> Open file (index.php) in the browser with "http://localhost/" + file location path
+    eg. http://localhost/x/index.php
+
+How To Test pay360client?
+0. pay.360.my run cmd > php artisan serve --host 0.0.0.0 --port 82
+1. index.php
+    > Reference ID : [insert integer, every time test must be unique number]
+    > Mode : [if choose Credit Card, follow step 2.1]
+             [if choose Duitnow B2B/B2C, follow step 2.2]
+    > Email : [insert valid email address]
+    > Success/Cancel/Status URL : [http://localhost/ + particular files location]
+                                    eg. if the success_page file (success_page.php) located in x/success_page.php, then type http://localhost/x/success_page.php in the Success URL box
+
+2.1 Mode: Credit Card
+    > What to fill in?
+        >> Email: [insert valid email address]
+        >> Payment Method: Card
+        >> Card Information: 4242 4242 4242 4242
+        >> MM/YY: 12/34
+        >> CVC: 567
+        >> Cardholder: [insert any string]
+    > Redirect to success_page
+
+2.2 Mode: Duitnow
+    > Select Bank: PYN Bank A
+    > Username: user1
+    > Password: password1
+    > Press Submit Payment - Confirm
+    > Redirect to 404 page
--- src/index.php
+++ src/index.php
@@ -39,14 +39,37 @@
         </tr>
         <tr>
             <td>Mode : </td>
-            <td><select id="mode" name="mode" required>
+            <td><select id="mode" name="mode" required onchange="showPaymentInfo()">
                 <option value="">Select Mode</option>
-                <!-- <option value="credit_card">Credit Card</option> -->
                 <option value="credit_card">Credit Card</option>
                 <option value="duitnow@b2b">DuitNow B2B</option>
                 <option value="duitnow@b2c">DuitNow B2C</option>
             </select></td>
         </tr>
+
+        <tr id="creditCardInfo" style="display:none;">
+            <td></td>
+            <td style="border: 1px solid black;">
+                !!! Note down this for later testing:
+                <br>>> Email: [insert valid email address]
+                <br>>> Payment Method: Card
+                <br>>> Card Information: 4242 4242 4242 4242
+                <br>>> MM/YY: 12/34
+                <br>>> CVC: 567
+                <br>>> Cardholder: [insert any string]
+            </td>
+        </tr>
+
+        <tr id="duitNowInfo" style="display:none;">
+        <td></td>
+            <td style="border: 1px solid black;">
+                !!! Note down this for later testing:
+                <br>>> Select Bank: PYN Bank A
+                <br>>> Username: user1
+                <br>>> Password: password1
+            </td>
+        </tr>
+
         <tr>
             <td>Email : </td>
             <td><input type="text" id="email" name="email" required></td>
@@ -57,7 +80,7 @@
         </tr>
         <tr>
             <td></td>
-            <td>Eg. http://localhost/test_pay360/success_url</td>
+            <td>- eg. http://localhost/pay360client/success_url</td>
         </tr>
         <tr>
             <td>Cancel URL : </td>
@@ -65,7 +88,7 @@
         </tr>
         <tr>
             <td></td>
-            <td>Eg. http://localhost/test_pay360/cancel_url</td>
+            <td>- eg. http://localhost/pay360client/cancel_url</td>
         </tr>
         <tr>
             <td>Status URL : </td>
@@ -73,7 +96,7 @@
         </tr>
         <tr>
             <td></td>
-            <td>Eg. http://localhost/test_pay360/status_url</td>
+            <td>- eg. http://localhost/pay360client/status_url</td>
         </tr>
         <tr>
             <td><br>
@@ -82,3 +105,18 @@
         </tr>
     </table>
 </form>
+
+<script>
+function showPaymentInfo() {
+    var mode = document.getElementById("mode").value;
+    
+    document.getElementById("creditCardInfo").style.display = "none";
+    document.getElementById("duitNowInfo").style.display = "none";
+    
+    if (mode == "credit_card") {
+        document.getElementById("creditCardInfo").style.display = "table-row";
+    } else if (mode == "duitnow@b2b" || mode == "duitnow@b2c") {
+        document.getElementById("duitNowInfo").style.display = "table-row";
+    }
+}
+</script>
